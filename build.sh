c8aa70dd2b76fbd784aea171b513bf4b9461b9cb
Stephen Meriwether <smeriwether@hey.com>
2021-05-05 08:53:17 -0400

    Move config files to a config folder

--- build.sh
+++ build.sh
@@ -2,10 +2,7 @@
 
 rm -rf ./site
 
-npx postcss ./styles.css -o ./site/styles.css
-
-cp index.html ./site/index.html
-cp 404.html ./site/404.html
+npx postcss --config=./config/postcss.config.js ./styles.css -o ./site/styles.css
 
 if [[ $NODE_ENV == "production" ]]; then
   for file in ./site/*.html; do
--- deploy.sh
+++ deploy.sh
@@ -6,25 +6,25 @@ NODE_ENV=production ./build.sh
 
 echo "Deploying..."
 
-scp -o StrictHostKeyChecking=no -rp $WEBSITE/site/* $WEBSITE_SERVER:/var/www/stephen.fyi
+scp -o StrictHostKeyChecking=no -rp ./site/* $WEBSITE_SERVER:/var/www/stephen.fyi
 if [ $? == 1 ]; then
   echo "Deploy failed"
   exit 1
 fi
 
-scp -o StrictHostKeyChecking=no -rp $WEBSITE/nginx/stephen.fyi $WEBSITE_SERVER:/etc/nginx/sites-enabled/stephen.fyi
+scp -o StrictHostKeyChecking=no -rp ./config/nginx/stephen.fyi $WEBSITE_SERVER:/etc/nginx/sites-enabled/stephen.fyi
 if [ $? == 1 ]; then
   echo "Deploy failed"
   exit 1
 fi
 
-scp -o StrictHostKeyChecking=no -rp $WEBSITE/nginx/smeriwether.com $WEBSITE_SERVER:/etc/nginx/sites-enabled/smeriwether.com
+scp -o StrictHostKeyChecking=no -rp ./config/nginx/smeriwether.com $WEBSITE_SERVER:/etc/nginx/sites-enabled/smeriwether.com
 if [ $? == 1 ]; then
   echo "Deploy failed"
   exit 1
 fi
 
-scp -o StrictHostKeyChecking=no -rp $WEBSITE/nginx/meriwether.io $WEBSITE_SERVER:/etc/nginx/sites-enabled/meriwether.io
+scp -o StrictHostKeyChecking=no -rp ./config/nginx/meriwether.io $WEBSITE_SERVER:/etc/nginx/sites-enabled/meriwether.io
 if [ $? == 1 ]; then
   echo "Deploy failed"
   exit 1
